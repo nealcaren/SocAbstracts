1fb51ec0fd2b9d8dd2a5d2ef63192a0f3af58284
Neal Caren <neal.caren@gmail.com>
2021-07-27 13:12:21 -0400

    Markdown improvements

--- asa.py
+++ asa.py
@@ -119,13 +119,16 @@ st.table(sdf.set_index('word'))
 
 st.markdown('### Small print')
 st.markdown('''This analysis is built on:
-*Data:*
+
+*Data:*     
 * [x] ASA conference abstracts from the ASA website from [x] to [y].
 * [y] articles published in sociology journals from Web of Science from [x] to [y].
+
 *Analysis:*
 * Conference/publication classification is based on logistic regression model after performing a grid search to find the best term frequency vectorizer paramaters.
 * Word colors are based on logistic regression coefficients.
-* The revisions options use word embeddings trained on the entire abstract corpus to find words with positive coefficients that are found in a similar context.    
+* The revisions options use word embeddings trained on the entire abstract corpus to find words with positive coefficients that are found in a similar context.
+
 *Software:*
 * Coding in Python using Jupyter Notebooks.
 * [scikit-learn](https://scikit-learn.org/stable/) for classification model.
